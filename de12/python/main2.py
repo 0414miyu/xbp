e03cc34172eae2b6c9bf89d4a0bf02d0c1ebcc6c
0414miyu <r202402101qw@jindai.jp>
2024-11-12 11:06:28 +0900

    20241112

--- de12/python/main2.py
+++ de12/python/main2.py
@@ -1,11 +1,21 @@
-name="aaa"
-waist=80
-age=38
+for i in range(1,4): #コロンが入っていることに注意
+    print(i,"人目") #タブでずらしていることに注意！
 
-print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+# 出力結果
+# 1 人目
+# 2 人目
+# 3 人目
 
-if waist>=85 or age>=40:
-    print(name,"さん、内臓脂肪蓄積注意です")
-else:
-    print(name,"さん、腹囲は問題ありません")
+    name=input("名前を教えて下さい")
+    waist=float(input("腹囲は？"))
+    age=int(input("年齢は？"))
+
+
+    print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
+
+    if waist>=85:
+        print(name,"さん、内臓脂肪蓄積注意です")
+    else:
+        print(name,"さん、腹囲は問題ありません")
 
